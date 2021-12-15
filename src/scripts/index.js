4862e8c5f89c7d73fc42ec2f09f9ade3dc88f702
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2021-12-15 20:04:48 +0700

    create section homepage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,40 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Typed from 'typed.js';
 import '../styles/css/main.css';
+
+// typing animation
+const typed = new Typed('.typing', {
+  strings: [
+    'Front-End Web Developer',
+    'Blogger',
+    'Content Creator',
+    'Freelancer',
+  ],
+  typeSpeed: 70,
+  backSpeed: 50,
+  loop: true,
+});
+
+// Custom Cursor
+let cursor1 = document.querySelector('.cursor__1');
+let cursor2 = document.querySelector('.cursor__2');
+
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + 'px';
+  cursor1.style.left = e.pageX + 'px';
+  cursor2.style.top = e.pageY + 'px';
+  cursor2.style.left = e.pageX + 'px';
+};
+
+document.querySelectorAll('a').forEach((links) => {
+  links.onmouseenter = () => {
+    cursor1.classList.add('active');
+    cursor2.classList.add('active');
+  };
+
+  links.onmouseleave = () => {
+    cursor1.classList.remove('active');
+    cursor2.classList.remove('active');
+  };
+});
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -27,6 +28,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/templates/index.html'),
       filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/public/'),
+        },
+      ],
     }),
   ],
 };
